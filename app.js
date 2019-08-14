cf743c3f6b64d2a70107b601ce84bb2ac4acd369
vlad <vlad@aporia.su>
2019-08-14 17:15:08 +0300

    add file apsolute path

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 var express     = require('express');
-const videoCore = require('./videoCore');
 var app         = express();
+var path        = require('path');
+const videoCore = require('./videoCore');
+var topModule   = module;
 
+while (topModule.parent)
+	topModule = topModule.parent;
 
+var appDir = path.dirname(topModule.filename);
 videoCore({
-	url: 'http://www.goodboydigital.com/pixijs/examples/12-2/',
+	url:      'http://www.goodboydigital.com/pixijs/examples/12-2/',
 	viewport: {
-		width: 800,               // sets the viewport (window size) to 800x600
-		height: 600
+		width:  3138,               // sets the viewport (window size) to 800x600
+		height: 252
 	},
 	//selector: '#container',     // crops each frame to the bounding box of '#container'
-	//left: 20, top: 40,          // further crops the left by 20px, and the top by 40px
-	//right: 6, bottom: 30,       // and the right by 6px, and the bottom by 30px
-	fps: 24,                    // saves 30 frames for each virtual second
-	duration: 5,               // for 20 virtual seconds
-	output: 'video.mp4'         // to video.mp4 of the current working directory
-}).then(function () {
+	fps:      24,                 // saves 30 frames for each virtual second
+	duration: 5,                  // for 20 virtual seconds
+	output:   'video.mp4',        // to video.mp4 of the current working directory
+}).then(function() {
+	// this is callback (after ffmpeg finish)
 	console.log('Done!');
+	console.log(appDir + '/video.mp4!');
 });
 module.exports = app;
